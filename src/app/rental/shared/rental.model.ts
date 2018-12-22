9e1ee2b5373863b16a60f18ad9d05e95d1b2c761
Jack <smalljjjack@users.noreply.github.com>
2018-12-22 12:11:41 -0500

    rental detail element

---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,12 @@
+export class Rental {
+  id: number;
+  title: string;
+  city: string;
+  street: string;
+  category: string;
+  image: string;
+  description: string;
+  dailyRate: number;
+  shared: boolean;
+  createdAt: string;
+}
