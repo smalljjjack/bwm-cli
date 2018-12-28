9779de5abf36484e3e51861fc8cdeb20acc7e2f9
Jack <smalljjjack@users.noreply.github.com>
2018-12-28 17:44:28 -0500

    worng pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent} from './common/header/head.component'
+import { HeaderComponent} from './common/header/head.component';
+import { RentalModule } from './rental/rental.module';
+import { RentalComponent } from './rental/rental.component';
+
+const routes: Routes = [
+  {path:'', redirectTo: '/rentals', pathMatch: 'full'},
+]
 
 @NgModule({
   declarations: [
@@ -11,8 +18,10 @@ import { HeaderComponent} from './common/header/head.component'
     HeaderComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RentalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rental/rental.module.ts
@@ -0,0 +1,45 @@
+import  { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule} from '@angular/router';
+import { MapModule } from '../common/map/map.module';
+
+import { RentalComponent } from './rental.component';
+import { RentalListComponent } from './rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental-list-item/rental-list-item.component'
+
+import { RentalService } from './shared/rental.service';
+import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import {UppercasePipe} from '../common/pipes/uppercase.pipe';
+
+const routes: Routes = [
+  {path:'rentals',
+   component: RentalComponent,
+   children:[
+     {path: "", component: RentalListComponent },
+     {path: ":rentalID", component: RentalDetailComponent },
+   ]
+ },
+]
+
+@NgModule({
+    declarations: [
+      RentalComponent,
+      RentalListComponent,
+      RentalListItemComponent,
+      RentalDetailComponent,
+      UppercasePipe,
+    ],
+    imports: [
+      CommonModule,
+      RouterModule.forChild(routes),
+      HttpClientModule,
+      MapModule,
+    ],
+
+    providers:[RentalService],
+})
+export class RentalModule {
+
+}
