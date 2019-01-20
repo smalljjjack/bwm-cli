3967c2d017688bec570558a38e1a41c1b8862de8
Jack <smalljjjack@users.noreply.github.com>
2019-01-20 15:37:28 -0500

    git added endpoints for creation rental

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Rental =  require('../models/rental');
-
+const User = require('../models/user');
 const UserCtrl = require('../controllers/user')
 
 const { normalizeErrors } = require('../helper/mongoose');
@@ -24,7 +24,24 @@ router.get('/:id', function(req, res){
         });
 });
 
-router.get('', function(req, res){
+router.post('', UserCtrl.authMiddleware, function(req, res){
+   const { title, city, street, category, image, shared, bedrooms, description, dailyRate } = req.body;
+   const user = res.locals.user;
+
+   const rental = new Rental({title, city, street, category, image, shared, bedrooms, description, dailyRate});
+   rental.user = user;
+   Rental.create(rental, function(err, newRental){
+
+     if(err){
+       return res.status(422).send({errors: normalizeErrors(err.errors)});
+     }
+
+     User.update({_id: user.id}, { $push: {rentals: newRental}}, function(){});
+     return res.json(newRental);
+   })
+})
+
+router.get('',function(req, res){
   const city = req.query.city;
   const query = city ? {city: city.toLowerCase()} : {};
 
